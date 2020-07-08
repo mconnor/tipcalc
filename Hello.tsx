ff14c8b17e598a1889864b1ffb094418e6c0403e
mike connor <mike@rocketnumber9.org>
2020-07-08 16:29:22 -0500

    hello

---
+++ Hello.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    View,
+    Text,
+} from 'react-native'
+
+const Hello = () => {
+    return (
+   
+            <View>
+                <Text>Howdy Partner</Text>
+            </View>
+      
+    )
+}
+
+export default Hello
