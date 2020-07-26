f931d5f44d0c2a02cdf8738a686c9594dc667100
mike connor <mike@rocketnumber9.org>
2020-07-26 17:25:16 -0500

    custom tip

--- App.tsx
+++ App.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 import Hello from './Hello'
 
+
+
+
 export default function App() {
+    const [copy, setCopy] = useState('');
+    const [tip, setTip] = useState(0.2)
+    const [customTip, setCustomTip] = useState<string | undefined>()
+
+    useEffect(() => {
+        if (customTip ) {
+            
+            setTip(parseFloat(customTip)/100)
+        } else  {
+            setTip(0)
+        }
+       
+    }, [customTip])
+
+
     return (
         <View style={styles.container}>
-            <View style={styles.view1}></View>
-            <View style={styles.view2}></View>
+            <Text>{copy && (Math.round(parseFloat(copy) * tip * 100) / 100).toFixed(2)}</Text>
+            <TextInput
+                value={copy}
+                style={styles.input}
+                keyboardType='numeric'
+                placeholder='0.00'
+                onChangeText={text => setCopy(text)} />
+            <View style={styles.buttonGroup}>
+                <Button
+                    title='10%'
+                    onPress={() => setTip(.10)}
+                />
+                <Button
+                    title='20%'
+                    onPress={() => setTip(.20)}
+                />
+                <Button
+                    title='30%'
+                    onPress={() => setTip(.30)}
+                />
+                <TextInput 
+                    style={styles.customInput}
+                    placeholder='custom tip'
+                    keyboardType='numeric'
+                    onChangeText={(customTip) => setCustomTip(customTip)}
+                    value={(tip *100).toString()}
+                />
+            </View>
         </View>
     );
 }
@@ -14,20 +58,27 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 20,
     },
-    view1: {
-        backgroundColor: '#ff00ff',
-        height: 100,
-        flex: 1,
+    input: {
+        height: 80,
+        width: '100%',
+        borderColor: '#333',
+        borderWidth: 1,
+        padding: 5,
     },
-    view2: {
-        backgroundColor: '#00ffff',
-        height: 100,
-        flex: 1,
-
+    customInput: {
+        height: 40,
+        width: 100,
+        borderColor: '#333',
+        borderWidth: 1,
+        padding: 5,
+    },
+    buttonGroup: {
+        flexDirection: 'row',
+        
     }
 });
