0125c8654923067fa170826b4f9c805b00fce162
mike connor <mike@rocketnumber9.org>
2020-08-07 19:22:51 -0500

    google fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, TextInput, Text, View } from 'react-native';
 import {
-    Container, Content, Left,
-    Right, Header, Icon, Body, Title, Button
+    StyleSheet, TextInput, Text, View, Alert
+} from 'react-native';
+import {
+    Container, Content, Button, Icon
 } from 'native-base';
-import { useFonts } from 'expo-font';
-//import { Ionicons } from '@expo/vector-icons';
-import Expo from 'expo';
+import {
+    useFonts,
+    Inter_900Black,
+  } from '@expo-google-fonts/inter';
+
 import { AppLoading } from 'expo';
+import Head from './ui/Head'
 
 
 
@@ -18,25 +22,9 @@ export default function App() {
     const [customTip, setCustomTip] = useState<string | undefined>()
     //const [isReady, setIsReady] = useState(false)
 
-    let [loaded, error] = useFonts({
-        // 'Roboto': require('native-base/Fonts/Roboto.ttf'),
-        // 'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-        'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
-     });
-
-     
-
-
-    // useEffect(() => {
-    //     async function loadFonts() {
-    //         await Font.loadAsync({
-    //             Roboto: require('native-base/Fonts/Roboto.ttf'),
-    //             Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-    //             ...Ionicons.font,
-    //         });
-    //         setIsReady(true);
-    //     }
-    // }, []);
+    let [isLoaded, error] = useFonts({
+        Inter_900Black,
+    });
 
 
 
@@ -51,72 +39,87 @@ export default function App() {
     }, [customTip])
 
     //
-if (!loaded) {
-    return  <AppLoading />;
-} else  {
+    if (!isLoaded) {
+        return <AppLoading />;
+    } else {
 
 
-    return (
-          
-                <Container>
-                    <Header>
-                        <Left>
-                            <Button transparent>
-                                <Icon name='arrow-back' />
-                            </Button>
-                        </Left>
-                        <Body>
-                            <Title>Header</Title>
-                        </Body>
-                        <Right>
-                            <Button transparent>
-                                <Icon name='menu' />
-                            </Button>
-                        </Right>
-                    </Header>
-                    <Content padder>
-                        <View style={styles.container}>
-                            <Text>{copy && (Math.round(parseFloat(copy) * tip * 100) / 100).toFixed(2)}</Text>
-                            <TextInput
-                                value={copy}
-                                style={styles.input}
-                                keyboardType='numeric'
-                                placeholder='0.00'
-                                onChangeText={text => setCopy(text)} />
-                            <View style={styles.buttonGroup}>
-                                <Button
-                                    onPress={() => setTip(.10)}
-                                >
-                                    <Text style={styles.btnStyle}>
-                                        '10%'
-                                    </Text></Button>
-                                <Button
-                                    onPress={() => setTip(.20)}
-                                ><Text>'20%'</Text></Button>
-                                <Button
-                                    onPress={() => setTip(.30)}
-                                ><Text>'30%'</Text></Button>
-                                <TextInput
-                                    style={styles.customInput}
-                                    placeholder='custom tip'
-                                    keyboardType='numeric'
-                                    onChangeText={(customTip) => setCustomTip(customTip)}
-                                    value={(tip * 100).toString()}
-                                />
-                            </View>
+        return (
+
+            <Container>
+                <Head />
+                <Content padder>
+                    <View style={styles.container}>
+                        <View>
+                        <Button
+                            warning
+                            onPress={() => Alert.alert(
+                                'Warning', 
+                                'erase iphone now?',
+                                    [
+                                        {text: 'OK',
+                                            onPress: ()=> console.log('hit ok')
+                                        },
+                                        {text: 'Cancel',
+                                            onPress: ()=> console.log('hit ok')
+                                        },
+                                    ]
+                                )}
+                        >
+                            <Icon name='beer' />
+                            <Text>Take a Break!</Text>
+                        </Button>
                         </View>
-                    </Content>
-                </Container>
-                 
-    );
-}
+                        
+                        <Text>{copy && (Math.round(parseFloat(copy) * tip * 100) / 100).toFixed(2)}</Text>
+                        <TextInput
+                            value={copy}
+                            style={styles.input}
+                            keyboardType='numeric'
+                            placeholder='0.00'
+                            onChangeText={text => setCopy(text)} />
+                        <View style={styles.buttonGroup}>
+                            <Button
+                                onPress={() => setTip(.10)}
+                            >
+                                <Text style={styles.btnStyle}>
+                                    '10%'
+                                    </Text></Button>
+                            <Button
+                                onPress={() => setTip(.20)}
+                            ><Text>'20%'</Text></Button>
+                            <Button
+                                onPress={() => setTip(.30)}
+                            ><Text>'30%'</Text></Button>
+                            <TextInput
+                                style={styles.customInput}
+                                placeholder='custom tip'
+                                keyboardType='numeric'
+                                onChangeText={(customTip) => setCustomTip(customTip)}
+                                value={(tip * 100).toString()}
+                            />
+                        </View>
+                    </View>
+                </Content>
+            </Container>
+
+        );
+    }
 }
 const styles = StyleSheet.create({
+    header: {
+        marginTop: 60,
+    },
     container: {
         flex: 1,
+        flexDirection: 'column',
         backgroundColor: '#fff',
         alignItems: 'center',
-   
+        fontFamily: ' Inter_900Black',
+
+    },
+    centerChild:  {
+        justifyContent:'center'
     },
     input: {
         height: 80,
@@ -137,7 +140,7 @@ const styles = StyleSheet.create({
 
     },
     btnStyle: {
-        fontFamily: 'Inter-SemiBoldItalic',
+        
         color: '#fff',
     }
 });
